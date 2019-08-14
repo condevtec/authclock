208c3384f78fa8a94bd4af56dfc6f3ef86904f80
Reimund Klain <reimund.klain@condevtec.de>
2019-08-15 00:23:44 +0200

    working impl

---
+++ app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import uuid
+import datetime as dt
+from flask import Flask, jsonify, session, abort
+from werkzeug.middleware.proxy_fix import ProxyFix
+from flask_httpauth import HTTPBasicAuth
+
+import onetimepass
+
+
+app = Flask(__name__, instance_relative_config=True)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or uuid.uuid4().hex
+app.wsgi_app = ProxyFix(app.wsgi_app)
+
+totop_auth = HTTPBasicAuth(realm="totp")
+
+@app.route('/auth/totp')
+@totop_auth.login_required
+def auth_totp():
+    return jsonify({"auth": session.get("auth"), "expire_at": session.get("expire_at")})
+
+
+@totop_auth.verify_password
+def verify_password(username, password):
+    if "expire_at" in session:
+        if dt.datetime.strptime(session.get("expire_at"), "%Y-%m-%dT%H:%M:%S.%fZ") <= dt.datetime.utcnow():
+            session.clear()
+            return False
+        else:
+            return True
+    check = onetimepass.valid_totp(password, "ADD_SECRET_HERE")
+    if check:
+        session["auth"] = "totp"
+        session["expire_at"] = (dt.datetime.utcnow() + dt.timedelta(hours=24)).isoformat()+"Z"
+    return check
