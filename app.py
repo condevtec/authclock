d64742d8034b1c33c2ec856cf01aa477f14391cd
Reimund Klain <reimund.klain@condevtec.de>
2019-08-15 12:42:25 +0200

    json response

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import uuid
 import datetime as dt
-from flask import Flask, jsonify, session, abort
+from flask import Flask, jsonify, session, request
 from werkzeug.middleware.proxy_fix import ProxyFix
 from flask_httpauth import HTTPBasicAuth
 
 import onetimepass
-
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
@@ -21,7 +18,13 @@ totop_auth = HTTPBasicAuth(realm="totp")
 @app.route('/auth/totp')
 @totop_auth.login_required
 def auth_totp():
-    return jsonify({"auth": session.get("auth"), "expire_at": session.get("expire_at")})
+    return jsonify(
+        {
+            "auth": session.get("auth"),
+            "expire_at": session.get("expire_at"),
+            "domain": request.host
+        }
+    )
 
 
 @totop_auth.verify_password
@@ -35,5 +38,11 @@ def verify_password(username, password):
     check = onetimepass.valid_totp(password, app.config['TOTP_SECRET'])
     if check:
         session["auth"] = "totp"
-        session["expire_at"] = (dt.datetime.utcnow() + dt.timedelta(hours=24)).isoformat()+"Z"
+        session["expire_at"] = (dt.datetime.utcnow() + dt.timedelta(hours=24)).isoformat() + "Z"
+        session["domain"] = request.host
     return check
+
+
+@totop_auth.error_handler
+def unauthorized():
+    return jsonify({"error": "unauthorized"})
